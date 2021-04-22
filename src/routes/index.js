654e59faf620bf589c80ab26174c8b086ece7844
Agustin133 <78511524+Agustin133@users.noreply.github.com>
2021-04-21 22:38:21 -0300

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post('/register', (req, res) => {
+    const { email, password } = req.body;
+    req.session.user_data = {email, password};
+    req.flash('success', 'Now You are Registered')
+    res.redirect('/profile');
+});
+
+router.get('/profile', (req, res) => {
+    const user = req.session.user_data;
+
+    if(user){
+        res.render('profile', {
+            user
+        });
+    }
+    if(!user){
+        res.redirect('/');
+    }
+});
+
+
+router.get('/logout', (req, res) => {
+    //delete req.session.user_data;
+    req.session.destroy();
+    res.redirect('/')
+});
+
+module.exports = router;
